76801a1601da7c8917595b7f9b6e959b25cb56e9
RuuushB <w.wait@yandex.ru>
2022-08-01 17:13:59 +0300

    Delete test_parser.py

--- test_parser.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_busket(browser):
-    browser.get(link)
-    x = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
-    assert x, "No such element found"
